0777500327623d43ea906603aa4507b42b61487c
Frederik Blichert Klovborg <fbk@moodagent.com>
2020-01-08 20:37:15 +0100

    dev server setup

---
+++ packages/server/src/server.ts
@@ -0,0 +1,33 @@
+import { Open5eMonster } from '@rpg-tools/open5e'
+import bodyParser from 'body-parser'
+import express from 'express'
+
+const main = async () => {
+  const app = express()
+
+  app.use(bodyParser.urlencoded({ extended: true }))
+
+  app.get('/', async (req, res) => {
+    const open5eMonster = new Open5eMonster()
+
+    const wolves = await open5eMonster.getMonstersByName('wolf')
+
+    const monsters = wolves.map(wolf => {
+      return {
+        name: wolf.name,
+        slug: wolf.slug,
+        challengeRating: wolf.challenge_rating,
+      }
+    })
+
+    res.send({ count: wolves.length, monsters })
+  })
+
+  const port = process.env.PORT || 4000
+
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+  })
+}
+
+main()
