b253ba2b0637957b3cba05b69be167644881abba
Frederik Klovborg <klovborg@gmail.com>
2020-01-08 23:10:24 +0100

    init discord bot

---
+++ packages/discord-bot/README.md
@@ -0,0 +1,14 @@
+# @rpg-tools/discord-bot
+
+Private Discord bot coded with [discord.js](https://discord.js.org/#/)
+
+Local config file is required in the `src` directory. the file should look like this:
+
+```json
+{
+  "prefix": "!",
+  "token": "you-token-goes-here"
+}
+```
+
+The token can be obtained in the [Discord Developer Portal](https://discordapp.com/developers/)
---
+++ packages/discord-bot/src/index.ts
@@ -0,0 +1,28 @@
+import { Open5eMonster } from '@rpg-tools/open5e'
+import Discord from 'discord.js'
+
+import { token } from './config.json'
+
+const client = new Discord.Client()
+
+client.once('ready', () => {
+  console.log('Ready!')
+})
+
+client.login(token)
+
+client.on('message', async msg => {
+  console.log(msg.content)
+
+  if(msg.content === '!ping') {
+    msg.channel.send('Pong!!!')
+  }
+
+  if(msg.content === '!wolf') {
+    const o5e = new Open5eMonster()
+    
+    const wolfData = await o5e.getMonsterBySlug('wolf')
+
+    msg.channel.send(`I've found a ${wolfData.name} that has ${wolfData.hit_points} hit points`)
+  }
+})
